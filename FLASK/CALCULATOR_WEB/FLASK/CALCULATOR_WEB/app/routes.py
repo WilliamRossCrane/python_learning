12260bef798e9fe3459dc475a9b0321ce0f3fb35
William Crane <wcran7@gmail.com>
2026-06-11 17:01:06 +1000

    Feat(SIMPLECALC - Route): Added logic using flask and routes with the calculate method, added a display box to the index.html

--- FLASK/CALCULATOR_WEB/FLASK/CALCULATOR_WEB/app/routes.py
+++ FLASK/CALCULATOR_WEB/FLASK/CALCULATOR_WEB/app/routes.py
@@ -1,7 +1,27 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    num1 = float(request.form["num1"])
+    num2 = float(request.form["num2"])
+    operation = request.form["operation"]
+
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        result = num1 / num2
+    else:
+        result = "Invalid operation"
+
+    return render_template("index.html", result=result)
