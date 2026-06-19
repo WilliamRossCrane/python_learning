5136186b762578196e758ea2fca5ddec1a267f00
William Crane <wcran7@gmail.com>
2026-06-19 16:43:03 +1000

    Feat(Creature Model): This commit adds the Creature model. The model defines what information each creature will store in the database, including its name, element, level, and rarity. It also includes a to_dict() method, which converts the creature into JSON-friendly data for the REST API.

--- FLASK/createdex_REST/createdex.py
+++ FLASK/createdex_REST/createdex.py
@@ -1,8 +1,10 @@
 from app import app, db
+from app.models import Creature
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {
         "db": db,
+        "Creature": Creature,
     }
