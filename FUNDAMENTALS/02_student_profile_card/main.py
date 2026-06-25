66c95028b0d4653fffd9dc84714cdc56444e31f4
William Crane <wcran7@gmail.com>
2026-06-26 09:02:05 +1000

    Feat(02): create student profile card project

---
+++ FUNDAMENTALS/02_student_profile_card/main.py
@@ -0,0 +1,40 @@
+# 02 Student Profile Card
+# This program uses variables to store information.
+
+# To run this program:
+# 1. Open the terminal.
+# 2. Move into this folder.
+# 3. Type:
+#    python3 main.py
+
+# A variable stores information.
+# Think of a variable like a labelled box.
+
+# This variable stores text.
+# Text in Python is called a string.
+student_name = "Will"
+
+# This variable stores a whole number.
+# Whole numbers are called integers.
+year_level = 8
+
+# These variables also store strings.
+favourite_subject = "Digital Technologies"
+favourite_game = "Pokemon"
+learning_goal = "get better at Python"
+
+# print() shows information in the terminal.
+print("STUDENT PROFILE CARD")
+print("--------------------")
+
+# We can print labels and variables together.
+# The comma lets us print more than one thing.
+print("Name:", student_name)
+print("Year Level:", year_level)
+print("Favourite Subject:", favourite_subject)
+print("Favourite Game:", favourite_game)
+print("Learning Goal:", learning_goal)
+
+# This final line uses the information stored in a variable.
+print()
+print(student_name, "is learning how variables work in Python.")
