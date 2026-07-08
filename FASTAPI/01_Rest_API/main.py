5a01740239f4d886c5959e4f5037e23b6c7806b6
William Crane <wcran7@gmail.com>
2026-07-08 18:02:02 +1000

    Feat(POST & GET): POST items into the items list with a curl command then retrieve them with GET via there ID in the array and updating the Curl URL

--- FASTAPI/01_Rest_API/main.py
+++ FASTAPI/01_Rest_API/main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return item
+
+# Test this POST route in the terminal with:
+# curl -X POST -H "Content-Type: application/json" "http://127.0.0.1:8000/items?item=apple"
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    item = items[item_id]
+    return item
