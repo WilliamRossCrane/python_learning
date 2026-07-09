9c76ee1c88d40bcebc3262a2587cf934d761d3c4
William Crane <wcran7@gmail.com>
2026-07-09 10:31:00 +1000

    Feat(Request & Path Parameters): Learnt how to update the payload so it has parameters instead of just being a string.

--- FASTAPI/01_Rest_API/main.py
+++ FASTAPI/01_Rest_API/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    test: str
+    is_done: bool = False
+
 
 items = []
 
@@ -9,15 +15,20 @@ def root():
     return {"message": "Hello, World!"}
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return item
 
 # Test this POST route in the terminal with:
 # curl -X POST -H "Content-Type: application/json" "http://127.0.0.1:8000/items?item=apple"
 
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+
 @app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
